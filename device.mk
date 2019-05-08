3d25b3866f17b0728343f8ed7340119421c8125e
POPIPIG233666 <2212848813@qq.com>
2019-05-08 10:30:01 -0400

    wayne: Drop trustzone check
    * Due to users who have no brain
    * Those who trigger arb
    * RIP

--- device.mk
+++ device.mk
@@ -356,10 +356,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
-# Recovery
-PRODUCT_PACKAGES += \
-    librecovery_updater_wayne
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
