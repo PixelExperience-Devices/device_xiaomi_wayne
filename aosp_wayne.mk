4788edea084b593752edf0088988c990a4493afe
Kyriakos Amvrosidis <amkirk94@yahoo.gr>
2019-09-07 05:31:04 +0300

    Delete duplicate
    The boot animation resolution was set twice in the same file.

--- aosp_wayne.mk
+++ aosp_wayne.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Set Boot Animination Resolution
-TARGET_BOOT_ANIMATION_RES := 2140
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
