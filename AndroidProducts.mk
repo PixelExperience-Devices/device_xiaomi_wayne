717f319130aaddc2fb213f41cb29e433fb59b1c0
RandomRetard4676 <randomretard4676@gmail.com>
2020-02-19 16:18:25 +0000

    wayne : PixelExperience Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # inherit MI 6X lineage device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/carbon_wayne.mk
+    $(LOCAL_DIR)/aosp_wayne.mk
 
 COMMON_LUNCH_CHOICES := \
-    carbon_wayne-eng \
-    carbon_wayne-userdebug \
-		carbon_wayne-user
+    aosp_wayne-eng \
+    aosp_wayne-userdebug \
+		aosp_wayne-user
